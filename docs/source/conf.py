e2d7dd8d9bec1cb4df039ed733498d1eb6738b61
SWijnands01 <113234176+SWijnands01@users.noreply.github.com>
2024-03-04 13:17:53 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ templates_path = ['_templates']
     
 # -- Options for HTML output
 
-html_theme = 'alabaster'
+html_theme = 'sphinxawesome_theme'
 html_logo = '../Logo.jpg'
 
 html_theme_options = {
